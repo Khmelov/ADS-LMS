3ea99a4dc7c71144e5f3656d846ecbad77b496ca
romnsrg-dev <romnsrg.dev@gmail.com>
2025-05-06 15:24:51 +0300

    feat: done lesson 9

--- src/by/it/group351051/belsky/lesson09/ListA.java
+++ src/by/it/group351051/belsky/lesson09/ListA.java
@@ -5,33 +5,103 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * Класс ListA - реализация списка без использования стандартных библиотек.
+ * @param <E> тип элементов списка
+ */
 public class ListA<E> implements List<E> {
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
+
+    // Константа для начальной емкости массива
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // Массив для хранения элементов
+    private Object[] elements;
+
+    // Текущий размер списка
+    private int size;
+
+    /**
+     * Конструктор по умолчанию, инициализирует массив с начальной емкостью.
+     */
+    public ListA() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
 
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     //////               Обязательные к реализации методы             ///////
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Возвращает строковое представление списка.
+     * @return строка, представляющая элементы списка
+     */
     @Override
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
+    /**
+     * Добавляет элемент в конец списка.
+     * @param e элемент для добавления
+     * @return true, если элемент успешно добавлен
+     */
     @Override
     public boolean add(E e) {
-        return false;
+        ensureCapacity();
+        elements[size++] = e;
+        return true;
     }
 
+    /**
+     * Удаляет элемент по индексу.
+     * @param index индекс элемента для удаления
+     * @return удалённый элемент
+     */
     @Override
     public E remove(int index) {
-        return null;
+        checkIndex(index);
+        E removedElement = (E) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+        return removedElement;
     }
 
+    /**
+     * Возвращает количество элементов в списке.
+     * @return размер списка
+     */
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    // Увеличивает емкость массива при необходимости
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+    }
+
+    // Проверяет индекс на корректность
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
     }
 
     /////////////////////////////////////////////////////////////////////////
