6009cccdce512a30b59acc6f08c403d4cdedd92e
Alex <Alex@test.com>
2026-06-06 21:24:15 +0300

    lesson9

--- src/by/it/group451051/shiman/lesson09/ListA.java
+++ src/by/it/group451051/shiman/lesson09/ListA.java
@@ -7,146 +7,293 @@ import java.util.ListIterator;
 
 public class ListA<E> implements List<E> {
 
-    //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
+    // Внутреннее хранилище элементов списка
+    private Object[] elements;
+    // Текущее количество элементов в списке
+    private int size;
 
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
-    //////               Обязательные к реализации методы             ///////
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
+    // Конструктор по умолчанию: создаём массив начальной ёмкости 10
+    public ListA() {
+        this.elements = new Object[10];
+        this.size = 0;
+    }
+
+    // Вспомогательный метод для увеличения ёмкости массива при необходимости
+    private void grow() {
+        if (size == elements.length) {
+            // Увеличиваем размер массива в 1.5 раза
+            int newCapacity = elements.length + (elements.length >> 1);
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+    }
+
+    // Возвращает строковое представление списка в виде [элемент1, элемент2, ...]
     @Override
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(elements[i]);
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
+    // Добавляет элемент в конец списка
     @Override
     public boolean add(E e) {
-        return false;
+        grow();               // убеждаемся, что есть место
+        elements[size++] = e; // сохраняем элемент и увеличиваем размер
+        return true;
     }
 
+    // Удаляет элемент по индексу и возвращает его
     @Override
     public E remove(int index) {
-        return null;
+        // Проверка корректности индекса
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        @SuppressWarnings("unchecked")
+        E oldValue = (E) elements[index];
+        // Количество элементов, которые нужно сдвинуть влево
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        // Обнуляем последнюю ссылку и уменьшаем размер
+        elements[--size] = null;
+        return oldValue;
     }
 
+    // Возвращает количество элементов в списке
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
-    //////               Опциональные к реализации методы             ///////
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
+    // ---------- Остальные методы интерфейса List (опциональные реализации) ----------
 
     @Override
     public void add(int index, E element) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        grow();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
         return false;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        @SuppressWarnings("unchecked")
+        E oldValue = (E) elements[index];
+        elements[index] = element;
+        return oldValue;
     }
-
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
-
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (E) elements[index];
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        boolean modified = false;
+        for (E e : c) {
+            if (add(e)) {
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        int numNew = c.size();
+        if (numNew == 0) {
+            return false;
+        }
+        // Убеждаемся, что хватит места
+        while (size + numNew > elements.length) {
+            grow();
+        }
+        // Сдвигаем элементы, освобождая место для новой коллекции
+        System.arraycopy(elements, index, elements, index + numNew, size - index);
+        // Вставляем элементы из коллекции
+        int i = index;
+        for (E e : c) {
+            elements[i++] = e;
+        }
+        size += numNew;
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (c.contains(elements[i])) {
+                remove(i);
+                i--; // после удаления индекс смещается
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elements[i])) {
+                remove(i);
+                i--;
+                modified = true;
+            }
+        }
+        return modified;
     }
-
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException("subList not implemented");
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException("listIterator not implemented");
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        throw new UnsupportedOperationException("listIterator not implemented");
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size) {
+            a = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] result = new Object[size];
+        System.arraycopy(elements, 0, result, 0, size);
+        return result;
     }
 
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
-    ////////        Эти методы имплементировать необязательно    ////////////
-    ////////        но они будут нужны для корректной отладки    ////////////
-    /////////////////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////////////////
+    // Итератор (минимальная реализация)
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int cursor = 0;
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+                return (E) elements[cursor++];
+            }
+        };
     }
-
 }
